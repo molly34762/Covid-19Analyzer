b7d8efbd7fb7f7337476d3356489d97b8b20fadc
teetor <41592130+teetor@users.noreply.github.com>
2020-05-11 13:40:56 -0400

    updated download url for census.csv

--- retrieve_data.py
+++ retrieve_data.py
@@ -2,7 +2,7 @@ import wget
 
 url_confirmed = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 url_deaths = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
-url_census = "https://raw.githubusercontent.com/molly34762/Database-Project/progressAlpha/census_data.csv"
+url_census = "https://raw.githubusercontent.com/molly34762/Database-Project/master/census_data.csv"
 try:
     f = open("confirmed.csv")
     f.close()
